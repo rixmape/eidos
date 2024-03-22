911d591ae9b32f58a5a3130a5d02e378f76d468d
rixmape <rixdonninorecario.mape@bicol-u.edu.ph>
2024-03-22 12:17:36 +0800

    Update to store feedback in a dictionary

--- pages/3_📝_Survey.py
+++ pages/3_📝_Survey.py
@@ -7,24 +7,23 @@ from helpers import page_utils
 
 class SurveyPage:
     def __init__(self):
-        self.state = st.session_state
-        self.state.setdefault("feedback", [])
+        self.initialize_session_state()
         self.initialize_page()
         self.initialize_feedback_database()
 
         self.options = [
-            "Strongly Agree",
-            "Agree",
-            "Disagree",
             "Strongly Disagree",
+            "Disagree",
+            "Agree",
+            "Strongly Agree",
         ]
 
+    def initialize_session_state(self):
+        self.state = st.session_state
+        self.state.setdefault("feedback", {})
+
     def initialize_page(self):
-        page_utils.initialize_page(
-            "📝",
-            "Share your experience",
-            customize_style=False,
-        )
+        page_utils.initialize_page("📝", "Share your experience")
 
     def initialize_feedback_database(self, database_name="user_feedback"):
         if not firebase_admin._apps:
@@ -38,18 +37,15 @@ class SurveyPage:
     def handle_likert_questions(self):
         for question in self.state.config.survey["likert-scale"]:
             st.markdown(f"#### {question}")
-            value = st.radio(
+            col, _ = st.columns(2)
+            value = col.select_slider(
                 question,
                 self.options,
                 key=question,
+                value=self.options[-1],
                 label_visibility="collapsed",
             )
-            self.state.feedback.append(
-                {
-                    "question": question,
-                    "value": value,
-                }
-            )
+            self.state.feedback.update({question: value})
 
     def handle_open_questions(self):
         for question in self.state.config.survey["open-ended"]:
@@ -61,27 +57,18 @@ class SurveyPage:
                 max_chars=500,
                 label_visibility="collapsed",
             )
-            self.state.feedback.append(
-                {
-                    "question": question,
-                    "value": value,
-                }
-            )
+            self.state.feedback.update({question: value})
 
     def send_feedback(self):
+        st.divider()
         if st.button("Send Feedback"):
-            self.database.add(
-                {
-                    "feedback": self.state.feedback,
-                    "timestamp": firestore.SERVER_TIMESTAMP,
-                }
-            )
+            self.state.feedback["timestamp"] = firestore.SERVER_TIMESTAMP
+            self.database.add(self.state.feedback)
             st.success("Feedback sent successfully!")
 
     def run(self):
         self.handle_likert_questions()
         self.handle_open_questions()
-        st.divider()
         self.send_feedback()
 
 
