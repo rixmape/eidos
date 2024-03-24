a1625c31e2a9dae67defab1e8fbc1b73a3ced9e9
rixmape <rixdonninorecario.mape@bicol-u.edu.ph>
2024-03-24 08:57:36 +0800

    Add diagram at home page

--- main.py
+++ main.py
@@ -15,14 +15,19 @@ class HomePage:
         self.state.visited_home = True
 
     def initialize_page(self):
-        page_utils.initialize_page("🏠", "Learn philosophy with Eidos")
+        page_utils.initialize_page("🏠", "Welcome!")
 
     def load_config(self):
         with open("config.yaml", "r", encoding="utf-8") as f:
             self.config = yaml.safe_load(f)
 
     def display_welcome_message(self):
-        st.markdown(self.config["welcome_message"])
+        st.header(":red[Eidos] is your philosophy companion.")
+
+        st.markdown("<br>", unsafe_allow_html=True)
+        st.image("images/eidos-flow.png")
+
+        st.markdown(f"#### {self.config['welcome_message']}")
 
     def display_next_page_link(self):
         st.divider()
